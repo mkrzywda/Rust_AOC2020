23a07666041ee2009c36cf575dd09873fa056344
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-12-01 11:42:35 +0100

    Modify main function with pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::io::BufRead;
 mod day01;
 fn main() {
 
-	day01::day01();
+	let day: i32 = std::io::stdin()
+    .lock()
+    .lines()
+    .next()
+    .expect("stdin should be available")
+    .expect("couldn't read from stdin")
+    .trim()
+    .parse()
+    .expect("input was not an integer");
+
+	match day {
+		1 => {
+			println!("Day01!\n");
+			day01::day01();
+		},
+        _ => println!("Ain't special"),
+    }
 
 }
+
