1a660bdd0a4057b463e0d40aa83d12b167233e70
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-12-06 13:13:49 +0100

    Day06: Part2

--- src/day06.rs
+++ src/day06.rs
@@ -7,12 +7,17 @@ pub fn day06() {
     let mut any_yes = std::collections::HashSet::new();
     let mut any_yes_sets = Vec::new();
 
+    let mut all_yes = std::collections::HashSet::new();
+    let mut all_yes_sets = Vec::new();
+
     let mut set_initialized = false;
 
     for line in lines {
         if line == "" {
+            all_yes_sets.push(all_yes);
             any_yes_sets.push(any_yes);
 
+            all_yes = std::collections::HashSet::new();
             any_yes = std::collections::HashSet::new();
 
             set_initialized = false;
@@ -25,13 +30,16 @@ pub fn day06() {
         }
         if !set_initialized {
             any_yes = yes_answers.iter().copied().collect();
+            all_yes = yes_answers.iter().copied().collect();
             set_initialized = true;
         }
         else {
             any_yes = any_yes.union(&yes_answers).cloned().collect();
+            all_yes = all_yes.intersection(&yes_answers).cloned().collect();
         }
     }
     any_yes_sets.push(any_yes);
+    all_yes_sets.push(all_yes);
 
     let mut sum = 0;
 
@@ -40,4 +48,11 @@ pub fn day06() {
     }
     println!("Part 1: {}", sum);
 
+    let mut all_yes_sum = 0;
+
+    for set in all_yes_sets {
+        all_yes_sum += set.len();
+    }
+
+    println!("Part 2: {}", all_yes_sum);
 }
